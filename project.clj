0a9dd163c2086e02fc39493ea1861242cbc4ef63
Ola Bini <ola.bini@gmail.com>
2012-07-28 19:28:15 -0500

    Initial checkin

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject gremlin "0.0.1"
+  :description "Implementation of TinkerPop Gremlin 2.0"
+  :url "https://github.com/olabini/gremlin-clj"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.tinkerpop.gremlin/gremlin-java "2.0.0"]]
+  )
